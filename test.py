e02f2bbcad3fb879a5da29a7933c2f08f1dbc245
ri-bhatta <57339760+ri-bhatta@users.noreply.github.com>
2023-11-27 15:39:57 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,4 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-
-class RequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/healthz':
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(b'OK')
-        else:
-            self.send_response(404)
-            self.end_headers()
-
 def run(server_class=HTTPServer, handler_class=RequestHandler, port=80):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print(f'Starting server on port {port}...')
-    httpd.serve_forever()
     print("*"*31)
     print("Checking Done")
     print("*"*31)
