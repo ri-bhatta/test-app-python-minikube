b34e279bea1c28208ba89189422866cd7140557b
ri-bhatta <57339760+ri-bhatta@users.noreply.github.com>
2023-11-27 17:48:29 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-import time
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class RequestHandler(BaseHTTPRequestHandler):
@@ -16,11 +15,6 @@ def run(server_class=HTTPServer, handler_class=RequestHandler, port=80):
     httpd = server_class(server_address, handler_class)
     print(f'Starting server on port {port}...')
     httpd.serve_forever()
-    print("completed")
 
 if __name__ == '__main__':
     run()
-    while True:
-        with open("/app/logs/output.log", "a") as f:
-            f.write(f"Generated log at {time.ctime()}\n")
-        time.sleep(5)  # Log every 5 seconds
