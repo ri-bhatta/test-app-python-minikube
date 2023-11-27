6036bc73e1535e5313a09ad083ba5013b189669b
ri-bhatta <57339760+ri-bhatta@users.noreply.github.com>
2023-11-27 16:09:18 +0530

    Update test.py

--- test.py
+++ test.py
@@ -15,6 +15,7 @@ def run(server_class=HTTPServer, handler_class=RequestHandler, port=80):
     httpd = server_class(server_address, handler_class)
     print(f'Starting server on port {port}...')
     httpd.serve_forever()
+    print("completed")
 
 if __name__ == '__main__':
     run()
