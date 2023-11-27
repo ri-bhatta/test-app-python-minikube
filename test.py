787d62becaf884b7b806fd12cbbc5a60beaf5017
ri-bhatta <57339760+ri-bhatta@users.noreply.github.com>
2023-11-27 16:58:01 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,19 @@
-with open("/app/logs/output.log", "w") as f:
-    f.write("Hello, It's Ritesh\nWelcome to you in test.py!\n")
+from http.server import BaseHTTPRequestHandler, HTTPServer
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/healthz':
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b'OK')
+        else:
+            self.send_response(404)
+            self.end_headers()
+def run(server_class=HTTPServer, handler_class=RequestHandler, port=80):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting server on port {port}...')
+    httpd.serve_forever()
+    print("completed")
 
-print("Hello, It's Ritesh\nWelcome to you in test.py!")
+if __name__ == '__main__':
+    run()
