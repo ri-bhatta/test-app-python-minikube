b25204c4dc8a4bd9e2257636320c68092b62440d
ri-bhatta <57339760+ri-bhatta@users.noreply.github.com>
2023-11-27 15:53:22 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
-def run():
-    print("*"*31)
-    print("Checking Done")
-    print("*"*31)
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/healthz':
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b'OK')
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+def run(server_class=HTTPServer, handler_class=RequestHandler, port=80):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting server on port {port}...')
+    httpd.serve_forever()
 
 if __name__ == '__main__':
-    print("Hello, It's Ritesh\nWelcome to you in test.py!")
-    print("*"*35)
-    print("\t\tlet's check the pod")
-    print("*"*35)
     run()
