16921e51096879068ae2419785a9bf17eaa9c684
505055128 <ritesh.bhattacharyya@capgemini.com>
2023-11-27 15:15:34 +0530

    file updated

--- dockerfile
+++ dockerfile
@@ -1,10 +1,6 @@
+# Dockerfile
+
 FROM python:3.9
-
 WORKDIR /app
-
 COPY test.py /app/test.py
-
-RUN useradd -m appuser
-USER appuser
-
 CMD ["python", "/app/test.py"]
--- test.py
+++ test.py
@@ -1,1 +1,30 @@
 print("Hello, It's Ritesh\nWelcome to you in test.py!")
+
+print("*"*35)
+print("\t\tlet's check the pod")
+print("*"*35)
+# test.py
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/healthz':
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b'OK')
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+def run(server_class=HTTPServer, handler_class=RequestHandler, port=80):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting server on port {port}...')
+    httpd.serve_forever()
+    print("*"*31)
+    print("Checking Done")
+    print("*"*31)
+
+if __name__ == '__main__':
+    run()
